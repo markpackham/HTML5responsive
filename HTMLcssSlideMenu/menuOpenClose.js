d67e9358546462387364b2b9141121cab0cee163
Mark Packham <markpackham1@gmail.com>
2017-12-02 10:06:07 +0000

    correct javascript

--- HTMLcssSlideMenu/menuOpenClose.js
+++ HTMLcssSlideMenu/menuOpenClose.js
@@ -1,9 +1,9 @@
 function openSlideMenu() {
     document.getElementById('side-menu').style.width = '250px';
-    document.getElementById('side-menu').style.marginLeft = '250px';
+    document.getElementById('main').style.marginLeft = '250px';
 }
 
 function closeSlideMenu() {
     document.getElementById('side-menu').style.width = '0px';
-    document.getElementById('side-menu').style.marginLeft = '0px';
+    document.getElementById('main').style.marginLeft = '0px';
 }
