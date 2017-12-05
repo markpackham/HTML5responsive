3988e4355e3f1cf19b1aa86233624a20a99730d8
Mark Packham <markpackham1@gmail.com>
2017-12-05 18:39:16 +0000

    sort out new html files

--- AdminBootstrapTheme/js/wysiwyg.js
+++ AdminBootstrapTheme/js/wysiwyg.js
@@ -1,1 +1,13 @@
+/*
+We get this from     <script src="https://cdn.ckeditor.com/4.7.3/standard/ckeditor.js"></script>
+in the html pages
+Found here https://cdn.ckeditor.com/
+ */
+
 CKEDITOR.replace('editor1');
+
+
+/*Show current year in Copyright */
+var date = new Date();
+var year = date.getFullYear();
+document.getElementById("theYear").innerHTML = year;
